39459af9b10a28cc60f229ec2842cb77be0e6578
Lib3r4tor <ivangonzalez0302@gmail.com>
2022-04-05 21:09:14 -0300

    Preparacion

---
+++ src/Funciones.h
@@ -0,0 +1,15 @@
+/*
+ * Funciones.h
+ *
+ *  Created on: 5 abr 2022
+ *      Author: Lauty
+ */
+
+#ifndef FUNCIONES_H_
+#define FUNCIONES_H_
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+#endif /* FUNCIONES_H_ */
---
+++ src/Funciones.s
@@ -0,0 +1,8 @@
+/*
+ * Funciones.s
+ *
+ *  Created on: 5 abr 2022
+ *      Author: Lauty
+ */
+#include "Funciones.h"
+
